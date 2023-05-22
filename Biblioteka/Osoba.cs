1644341e80b0118746c686b293680ea0e4b90502
Gregor Gran <grzegorz.swierk01@gmail.com>
2023-05-22 14:13:06 +0200

    Add project files.

---
+++ Biblioteka/Osoba.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Biblioteka
+{
+    public class Osoba : object
+    {
+        public string? Nazwisko; // znak ? pozwala na wartość null
+        public DateTime DataUrodzenia;
+        public List<Osoba> Dzieci = new();
+
+        public void WypiszWKonsoli()
+        {
+            Console.WriteLine($"{Nazwisko}, data urodzenia {DataUrodzenia:dddd}.");
+        }
+        public static Osoba Prokreacja(Osoba o1, Osoba o2)
+        {
+            Osoba dziecko = new()
+            {
+                Nazwisko = $"Dziecko osób {o1.Nazwisko} i {o2.Nazwisko}"
+            };
+
+            o1.Dzieci.Add(dziecko);
+            o2.Dzieci.Add(dziecko);
+
+            return dziecko;
+        }
+        public Osoba ProkreacjaZ(Osoba partner)
+        {
+            return Prokreacja(this, partner);
+        }
+        public static Osoba operator *(Osoba o1, Osoba o2)
+        {
+            return Osoba.Prokreacja(o1, o2);
+        }
+        public static int Silnia(int liczba)
+        {
+            if (liczba < 0)
+            {
+                throw new ArgumentException($"{nameof(liczba)} nie może być mniejsza od zera.");
+            }
+            return lokalnaSilnia(liczba);
+
+            int lokalnaSilnia(int lokalnaLiczba)
+            {
+                if (lokalnaLiczba < 1) return 1;
+                return lokalnaLiczba * lokalnaSilnia(lokalnaLiczba - 1);
+            }
+        }
+
+        public event EventHandler Krzycz;
+        public int PoziomZlosci;
+        public void Szturchnij()
+        {
+            PoziomZlosci++;
+            if (PoziomZlosci >= 3)
+            {
+                if (Krzycz != null)
+                {
+                    Krzycz(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
---
+++ Osoby/Program.cs
@@ -0,0 +1,22 @@
+﻿using Biblioteka;
+using static System.Console;
+
+
+var henryk = new Osoba { Nazwisko = "Henryk" };
+var maria = new Osoba { Nazwisko = "Maria" };
+var julia = new Osoba { Nazwisko = "Julia" };
+
+var dziecko1 = maria.ProkreacjaZ(henryk);
+dziecko1.Nazwisko = "Grzegorz";
+
+var dziecko2 = Osoba.Prokreacja(henryk,julia);
+
+WriteLine($"{maria.Nazwisko} ma {maria.Dzieci.Count} dzieci.");
+WriteLine($"{henryk.Nazwisko} ma {henryk.Dzieci.Count} dzieci.");
+WriteLine($"{julia.Nazwisko} ma {julia.Dzieci.Count} dzieci.");
+WriteLine(format: $"pierwsze dziecko {maria.Nazwisko} nazywa sie \"{maria.Dzieci[0].Nazwisko}\".");
+
+var dziecko3 = henryk * maria;
+
+WriteLine($"5! to {Osoba.Silnia(5)}");
+
